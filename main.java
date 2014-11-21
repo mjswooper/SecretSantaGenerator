49f64fbd9410aa0e9549afcd1bf231a0c418e627
mjswooper <admin@planned-escape.com>
2014-11-21 15:43:34 +1000

    Start allocation. Bug fix.
    Passing the arraylist needs to be Arraylisy<Person> as argument or
    'unsafe'.

--- main.java
+++ main.java
@@ -18,8 +18,6 @@
  */
 
 
-
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -30,60 +28,57 @@ import java.util.Arrays;
 
 public class main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("**************************** Program starting.... ****************************");
+        //      Debug:
+        //System.out.println("///////// Starting Object Test ////");
+        //      testObj();
+        //      testArrayList(testObj());
+        //System.out.println("/////////// Ending.////////////////");
 
-//		Debug:
-		System.out.println("///////// Starting Object Test ////");
-//		testObj();
-//		testArrayList(testObj());
-		System.out.println("/////////// Ending.////////////////");
-				
-		ArrayList<Person> santaList = new ArrayList<Person>(); //create arraylist to store reference to objects
+        ArrayList<Person> santaList = new ArrayList<Person>(); //create arraylist to store reference to objects
 
-		csvReader(santaList);
-		
-		allocate(santaList);
-		
-		for (int x =0; x < santaList.size();x++){
-			System.out.print(x + ":" + santaList.get(x) + " " + santaList.get(x).getName() + " " + santaList.get(x).getType() + " " + santaList.get(x).getCat() + 
-					" " + santaList.get(x).printPresents() + " " + santaList.get(x).totalPresents() + "\n");
-		}
-		
-		
-		
-		
-	}
+        csvReader(santaList);
 
-	/////////
-	//Testing
-	////////
-	
-	public static Person testObj(){
-		Person ss1 = new Person();
-		ss1.setName("swooper");
-		ss1.setType("A");
-		ss1.setCat("County");
-		System.out.println("Details: " + ss1.getName() + " " + ss1.getType() + " " + ss1.getCat());
-		ss1.givePresent("bob",0);
-		ss1.givePresent("yoyo",1);
-		ss1.givePresent("Magus",5);
-		System.out.println("Presents: " + ss1.printPresents());
-		System.out.println("Total: " + ss1.totalPresents());
-		return ss1;
-	}
-	
-	public static void testArrayList(Person testPerson){
-		ArrayList<Person> testList = new ArrayList<Person>();
-		testList.add(testPerson);
-		System.out.println(testList.get(0) + " " + testList.get(0).getName());
-	}
-	
-	//////////
-	//End test
-	//////////
+        allocate(santaList);
 
-	public static ArrayList csvReader(ArrayList santaList){
-		
+        for (int x =0; x < santaList.size();x++){
+            System.out.print(x + ":" + santaList.get(x) + " " + santaList.get(x).getName() + " " + santaList.get(x).getType() + " " + santaList.get(x).getCat() + 
+                " " + santaList.get(x).printPresents() + " " + santaList.get(x).totalPresents() + "\n");
+        }
+
+        System.out.println("**************************** Program finished ****************************");
+    }
+    /////////
+    //Testing
+    ////////
+
+    public static Person testObj(){
+        Person ss1 = new Person();
+        ss1.setName("swooper");
+        ss1.setType("A");
+        ss1.setCat("County");
+        System.out.println("Details: " + ss1.getName() + " " + ss1.getType() + " " + ss1.getCat());
+        ss1.givePresent("bob",0);
+        ss1.givePresent("yoyo",1);
+        ss1.givePresent("Magus",5);
+        System.out.println("Presents: " + ss1.printPresents());
+        System.out.println("Total: " + ss1.totalPresents());
+        return ss1;
+    }
+
+    public static void testArrayList(Person testPerson){
+        ArrayList<Person> testList = new ArrayList<Person>();
+        testList.add(testPerson);
+        System.out.println(testList.get(0) + " " + testList.get(0).getName());
+    }
+
+    //////////
+    //End test
+    //////////
+
+    public static ArrayList csvReader(ArrayList santaList){
+
         try {
             BufferedReader CSV = new BufferedReader(new FileReader(
                         "./SantaNames.csv"));
@@ -93,82 +88,73 @@ public class main {
             // Iterates through, sets current line and detects end
             // Also stops where ",," exists from excel empty cells. 
             // RULE: Assumes no blanks.
-            
+
             while ((currentLine = CSV.readLine()) != null
             && currentLine.equals(",,") == false) {
-                
+
                 //santaDetails will be what we rip off the csv. The rest is for the array use.
                 String[] arrInfo = currentLine.split(","); // split string into parts
-                
+
                 if (arrInfo.length != 3) {
                     System.out.println("CSV error. This line has empty cells(excel) and thus missing information.");
                     break;
                 }
-                
+
                 Person ss = new Person();
                 ss.setName(arrInfo[0]);   
                 ss.setType(arrInfo[1]); 
                 ss.setCat(arrInfo[2]); 
-                
+
                 santaList.add(ss); 
-             
+
             }
-            
+
             CSV.close(); // close off the CSV reader.
 
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } // end array list loop
-        
-        return santaList;
-	}
-	
-	
-	public static ArrayList allocate(ArrayList santaList){
-		
-		for (int y = 1; y<=6; y++) {//go across through the presents
-			
-			for (int x = 0; x < santaList.size(); x++) { //go down through the list of people
-				
-				if (santaList.get(x).getType() == "c" || 
-						santaList.get(x).getType() == "t" && y == 5 ||
-						santaList.get(x).getType() == "t" && y == 6 ){
-					
-					santaList.get(x).givePresent("none", y);						
-				}
-				else{
-					//pick a random number
-					int santaPick = 0 + (int) (Math.random() * ((santaList.size() - 1)));
-					int tally = 0;
-					
-					//Same category?
-					
-					
-						if (santaList.get(santaPick).getCat().equals(santaList.get(x).getCat()) == true &&
-								santaList.get(santaPick).totalPresents() <= 6){
-							
-							while (santaList.get(santaPick).getCat().equals(santaList.get(x).getCat()) == true &&
-									santaList.get(santaPick).totalPresents() <= 6){
-								santaPick++;
-								tally++;
-								
-								if (santaPick >= santaList.size())
-									santaPick = 0;
-								
-							}
-						}	
-						
-					}			
-					
-				}
-			}
-		
-		
-		return santaList;
-	}
-	
-	
 
-}	
+        return santaList;
+    }
+
+    public static ArrayList allocate(ArrayList<Person> santaList){
+        for (int y = 1; y<=6; y++) {//go across through the presents
+            for (int x = 0; x < santaList.size(); x++) { //go down through the list of people
+                if (santaList.get(x).getType() == "c" || 
+                santaList.get(x).getType() == "t" && y == 5 ||
+                santaList.get(x).getType() == "t" && y == 6 ){
+
+                    santaList.get(x).givePresent("none", y);                        
+                }
+                else{
+                    //pick a random number
+                    int santaPick = 0 + (int) (Math.random() * ((santaList.size() - 1)));
+                    int tally = 0;
+
+                    //Same category?
+
+                    if (santaList.get(santaPick).getCat().equals(santaList.get(x).getCat()) == true &&
+                    santaList.get(santaPick).totalPresents() <= 6){
+
+                        while (santaList.get(santaPick).getCat().equals(santaList.get(x).getCat()) == true &&
+                        santaList.get(santaPick).totalPresents() <= 6){
+                            santaPick++;
+                            tally++;
+
+                            if (santaPick >= santaList.size())
+                                santaPick = 0;
+
+                        }
+                    }   
+
+                }           
+
+            }
+        }
+
+        return santaList;
+    }
+}   
 
